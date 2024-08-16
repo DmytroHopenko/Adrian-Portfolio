1f54b61f48ffed4fb77f1b7ba993f675744dc0de
DmytroHopenko <harss320@gmail.com>
2024-08-16 12:57:00 +0300

    Adrian Portfolio

---
+++ components/Grid.tsx
@@ -0,0 +1,24 @@
+import { gridItems } from "@/data"
+import { BentoGrid, BentoGridItem } from "./ui/BentoGrid"
+
+export const Grid = () => {
+    return (
+        <section id="about">
+            <BentoGrid>
+                {gridItems.map(({id, title, description, img, className, imgClassName, titleClassName, spareImg}) => (
+                    <BentoGridItem 
+                    id={id}
+                    key={id}
+                    title={title}
+                    description={description}
+                    className={className}
+                    img={img}
+                    imgClassName={imgClassName}
+                    titleClassName={titleClassName}
+                    spareImg={spareImg}
+                    />
+                ))}
+            </BentoGrid>
+        </section>
+    )
+}
